ef69a87fccb46e7faf582d12f37f96c36fd7095f
Sam Croker(SASDEMO) <sam.croker@sas.com>
2017-06-13 14:25:34 -0400

    INcremental commit prior to moving to big cluster

--- RSwat_BasicFunctionality.R
+++ RSwat_BasicFunctionality.R
@@ -18,13 +18,37 @@ files <- unnest(data.frame(cas.table.fileInfo(session)))
 #list them according to size
 files[order(-files[7]),c(4,7)]
 
+largestFileName <- files[which.max(files[,7]),4]
+
+
 # One of the advantages of using R SWAT is that you get to choose location of execution. If the 
 # data sizes are small, it makes sense to use the regular R core for execution. However, if the data
 # sizes are extensive, it may be impossible to load the data into a local R data frame depending on 
-# available memory and resources.
+# available memory and resources.  This is especially useful in data science techniques where operations
+# on large datasets require heavy lifting but much of the meta analysis of results is contained in smaller 
+# datasets. 
 
-#Check status of the SAS Viya cluster. This command lists many statustes related to the cluster.
+#Check status of the SAS Viya cluster. This command lists many status related to the cluster.
 res <- cas.builtins.serverStatus(session) 
 #This returns an R list object that can be queried in the normal R way
 res$server
 
+# Local vs Remote Execution
+data(mtcars)
+object.size(mtcars)
+# Load the iris dataframe to CAS - it is small, but this is for demonstration purposes
+mtcars_cas <- as.casTable(session, mtcars)  
+object.size(mtcars_cas)
+
+#Load a file from remote filesystem to cas
+cas.table.loadTable(session,path=largestFileName,caslib="DemoData")
+
+tables <- unnest(data.frame(cas.table.tableInfo(session,caslib='DemoData')))
+
+cas.table.columnInfo(session,table ="INSIGHT_TOY4" )
+
+# Create a handler on the R side for the table on the CAS side
+insight <- defCasTable(session, table="INSIGHT_TOY4", groupby=c("Facility"))
+object.size(insight)
+
+groups <- cas.simple.summary(insight, subset=c("min", "max", "mean"))
