2ccb5b2ee64df881673230ecc8837d40cefa77e0
Sam Croker(SASDEMO) <sam.croker@sas.com>
2017-06-15 11:32:07 -0400

    Rectification

--- Airline2008Example.R
+++ Airline2008Example.R
@@ -2,6 +2,8 @@
 
 library(swat)
 library(tidyr)
+library(ggplot2)
+library(reshape2)
 
 # Connect to CAS, start a session using binary rather than REST transfer protocol
 session <- CAS('localhost', 5570, authfile='~./authinfo')
@@ -12,4 +14,29 @@ session <- CAS('localhost', 5570, authfile='~./authinfo')
 cas.sessionProp.setSessOpt(session,caslib='DemoData')
 #enable code completion
 options(cas.gen.function.sig=TRUE)
+
+#upload the csv into CAS in-memory table directly
+airline2008_h <- cas.read.csv(session, "~/2008.csv", tablenam="Air2008", replace=TRUE)
+# load the cas table into local R = not always a good idea:)
+airlocal_df <- to.casDataFrame(airline2008_h)
+
+#compare local vs remote object 
+object.size(airline2008_h)
+object.size(airlocal_df)
+
+
+cas.table.tableInfo(session,caslib='DemoData')
+
+head(airline2008_h)
+
+#Use CAS enabled R functions
+cas.simple.summary(airline2008_h)
+cas.simple.distinct(airline2008_h$UniqueCarrier)
+
+#take a look at the in-memory tables
+tables <- unnest(data.frame(cas.table.tableInfo(session,caslib='DemoData')))
+tables[c(1,16)]
+
+cas.table.columnInfo(session,table ="AIR2008" )
+
 
